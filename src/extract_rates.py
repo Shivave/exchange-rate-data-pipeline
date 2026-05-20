3369da5b063fd06b2b83a20fa0ea36dff8a2ae84
Shivave <161760115+Shivave@users.noreply.github.com>
2026-05-20 14:44:51 +0530

    Create extract_rates.py

---
+++ src/extract_rates.py
@@ -0,0 +1,6 @@
+├── src/
+│   └── extract_rates.py   <-- Your main Python script
+├── data/                  <-- Where our processed data will land
+├── .gitignore             <-- Automatically created by GitHub
+├── requirements.txt       <-- Lists the tools we need
+└── README.md              <-- The homepage of your project
